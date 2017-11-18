f66255508b23cb7c60ac8ceabbd9c710720d8aaf
Conciliabulle <malemennk@gmail.com>
2017-11-18 19:20:43 +0000

    fixed the image file name extraction and trained my basic model

--- model.py
+++ model.py
@@ -1,6 +1,11 @@
 import csv
 import cv2
 import numpy as np
+import ntpath
+
+def path_leaf(path):
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
 
 lines = []
 with open('./my_training_data/driving_log.csv') as csvfile:
@@ -12,14 +17,14 @@ images = []
 measurements = []
 for line in lines:
     source_path = line[0]
-    filename, tail = os.path.split(source_path)
+    filename = path_leaf(source_path)
     current_path = './my_training_data/IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
 
-print(filename)
+print('The file name is: ', filename)
 X_train = np.array(images)
 y_train = np.array(measurements)
 
@@ -33,7 +38,7 @@ model.add(Flatten(input_shape=(160,320,3)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=15)
 
 model.save('model.h5')
 
