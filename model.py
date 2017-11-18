058a75610d5a6fdb934ce344b2dc6d78d1f7f552
Conciliabulle <malemennk@gmail.com>
2017-11-18 17:25:14 +0000

    created model, basic to test everything is working, problem with exctraction of the image file name

---
+++ model.py
@@ -0,0 +1,42 @@
+import csv
+import cv2
+import numpy as np
+
+lines = []
+with open('./my_training_data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+        
+images = []
+measurements = []
+for line in lines:
+    source_path = line[0]
+    filename, tail = os.path.split(source_path)
+    current_path = './my_training_data/IMG/' + filename
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+print(filename)
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+print('Total number of training images: ', X_train.shape)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+
+model = Sequential()
+model.add(Flatten(input_shape=(160,320,3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+
+model.save('model.h5')
+
+
+
+
